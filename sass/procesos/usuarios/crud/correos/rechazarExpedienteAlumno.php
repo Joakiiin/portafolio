3b1bf92459cf123c4ec7a34a0793dfa7e3a4fec1
Joaquin Galindo Trejo <87915648+Joakiiin@users.noreply.github.com>
2023-06-12 12:53:17 -0600

    Update rechazarExpedienteAlumno.php

--- sass/procesos/usuarios/crud/correos/rechazarExpedienteAlumno.php
+++ sass/procesos/usuarios/crud/correos/rechazarExpedienteAlumno.php
@@ -13,14 +13,14 @@ try {
     $mail->isSMTP();
     $mail->Host = 'smtp.office365.com';
     $mail->SMTPAuth = true;
-    $mail->Username = 'joakiiin-14@live.com.mx';
-    $mail->Password = '5526653535telcel';
+    $mail->Username = 'CORREO';
+    $mail->Password = 'CONTRASEÑA';
     $mail->SMTPSecure = 'tls';
     $mail->Port = 587;
 
     // Configurar el mensaje
     $mail->CharSet = 'UTF-8';
-    $mail->setFrom('joakiiin-14@live.com.mx', 'Joaquin');
+    $mail->setFrom('CORREO', 'REMITENTE');
     $mail->addAddress($correo);
     $mail->Subject = 'Expediente Rechazado';
     $mail->Body = 'Estimado/a alumno/a,
